f5687f9c5cdd23d0bc0bb8ddcfeda3080b65c0b5
kotg <karwoo.tang@gft.com>
2018-04-03 10:24:52 +0100

    Added services

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-about',
@@ -8,9 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  goals: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) {
+    this.route.params.subscribe(res => console.log(res.id));
+  }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+  }
+
+  sendMeHome() {
+    this.router.navigate(['']);
   }
 
 }
